084bdbfc4292248d426d4fd6aec8c340bd6cf7f3
Brent Bumann <brent.bumann@aol.com>
2019-05-16 12:03:37 -0700

    Add helper methods for radix sort

--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -1,6 +1,22 @@
 function radixSort(arr) {
-
+  
 }
+
+const getDigitFrom = (num, place) =>{
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+};
+
+const getIntLength = num => {
+  return num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+};
+
+const getMaxDigits = nums => {
+  let maxDigits = 0;
+  for(let i = 0; i < nums.length; i++){
+    maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+  }
+  return maxDigits;
+};
 
 module.exports = {
     radixSort
