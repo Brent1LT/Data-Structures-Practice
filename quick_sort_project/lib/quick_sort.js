ef31cbe4ed47e60a2060c235f71dd6725ea487d1
Brent Bumann <brent.bumann@aol.com>
2019-05-15 22:36:57 -0700

    Finish quicksort

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,12 @@
 function quickSort(array) {
+  if(array.length <= 1) return array;
 
+  let pivot = array.shift();
+
+  let right = quickSort(array.filter(el => el >= pivot));
+  let left = quickSort(array.filter(el => el < pivot));
+
+  return [...left, pivot, ...right];
 }
 
 
