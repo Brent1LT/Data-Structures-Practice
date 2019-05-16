9b18f4589e0996feb3889306eaedd018df1cbfc9
Brent Bumann <brent.bumann@aol.com>
2019-05-16 15:44:43 -0700

    Finish counting sort

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,18 @@
 function countingSort(arr, max) {
+  let numbers = new Array(max + 1).fill(0);
+  let result = [];
+  for(let i = 0; i < arr.length; i++){
+    numbers[arr[i]] += 1;
+  }
 
+  for(let j = 0; j <= numbers.length; j++){
+    if(numbers[j]) {
+      for(let times = 0; times < numbers[j]; times++){
+        result.push(j);
+      }
+    } 
+  }
+  return result;
 }
 
 
