ea6bba7b3ec4c2fe3720bfbe9957f0be550b9283
Brent Bumann <brent.bumann@aol.com>
2019-05-17 15:35:28 -0700

    Finish Linked List project

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -22,67 +22,148 @@
 
 // TODO: Implement a Linked List Node class here
 class Node {
-    constructor(val) {
-
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 
 }
 
 // TODO: Implement a Singly Linked List class here
 class LinkedList {
-    constructor() {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
 
+  // TODO: Implement the addToTail method here
+  addToTail(value) {
+    if(!this.length){
+      let node = new Node(value);
+      this.head = node;
+      this.tail = node;
+    }else{
+      let temp = this.tail;
+      let node = new Node(value);
+      this.tail = node;
+      temp.next = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  // TODO: Implement the removeTail method here
+  removeTail() {
+    let removed = this.tail;
+    if(!this.size()) return undefined;
+    if(this.size() === 1){
+      this.head = null;
+      this.tail = null;
+    }else{
+      let currentNode = this.head;
+      while(currentNode.next !== this.tail){
+        currentNode = currentNode.next;
+      }
+      this.tail = currentNode;
+      this.tail.next = null;
+    }
+    this.length--;
+    return removed;
+  }
+
+  // TODO: Implement the addToHead method here
+  addToHead(val) {
+    let node = new Node(val);
+    if(!this.size()){
+      // this.head = node;
+      // this.tail = node;
+      [this.head, this.tail] = [node, node];
+    }else{
+      let temp = this.head;
+      this.head = node;
+      this.head.next = temp;
+    }
+    this.length++;
+    return this;
+  }
+
+  // TODO: Implement the removeHead method here
+  removeHead() {
+    if(!this.size()) return undefined;
+    let removed = this.head;
+    if(this.size() === 1){
+      [this.head, this.tail] = [null, null];
+    } else {
+      this.head = removed.next;
+    }
+    this.length --;
+    return removed;
+  }
+
+  // TODO: Implement the contains method here
+  contains(target) {
+    let currentNode = this.head;
+    let contained = false;
+    while(currentNode !== null){
+      if(target === currentNode.value) contained = true;
+      currentNode = currentNode.next;
+    }
+    return contained;
+  }
+
+  // TODO: Implement the get method here
+  get(index) {
+    if(index > this.size() - 1) return null;
+    let currentIndex = 0;
+    let currentNode = this.head;
+    while(currentIndex !== index){
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+    return currentNode;
+  }
+
+  // TODO: Implement the set method here
+  set(index, val) {
+    if (index > this.size() - 1) return false;
+    let node = this.get(index);
+    node.value = val;
+    return true;
+  }
+
+  // TODO: Implement the insert method here
+  insert(index, val) {
+    if (index > this.size() - 1) return false;
+
+    let prevNode;
+    if(!index) {
+      prevNode = this.get(0);
+    } else {
+      prevNode = this.get(index - 1);
     }
 
-    // TODO: Implement the addToTail method here
-    addToTail(val) {
+    let nextNode = prevNode.next;
+    let node = new Node(val);
+    [prevNode.next, node.next] = [node, nextNode];
+    this.length++;
+    return true;
+  }
 
-    }
+  // TODO: Implement the remove method here
+  remove(index) {
+    if (index > this.size() - 1) return undefined;
+    let prevNode = this.get(index - 1);
+    let removed = prevNode.next;
+    prevNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
 
-    // TODO: Implement the removeTail method here
-    removeTail() {
-
-    }
-
-    // TODO: Implement the addToHead method here
-    addToHead(val) {
-
-    }
-
-    // TODO: Implement the removeHead method here
-    removeHead() {
-
-    }
-
-    // TODO: Implement the contains method here
-    contains(target) {
-
-    }
-
-    // TODO: Implement the get method here
-    get(index) {
-
-    }
-
-    // TODO: Implement the set method here
-    set(index, val) {
-
-    }
-
-    // TODO: Implement the insert method here
-    insert(index, val) {
-
-    }
-
-    // TODO: Implement the remove method here
-    remove(index) {
-
-    }
-
-    // TODO: Implement the size method here
-    size() {
-
-    }
+  // TODO: Implement the size method here
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
