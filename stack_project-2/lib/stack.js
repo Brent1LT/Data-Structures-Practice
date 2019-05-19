cf5e283f324a661014a281edcf7abc343da6e369
Brent Bumann <brent.bumann@aol.com>
2019-05-18 22:40:53 -0700

    Finish stack problems

--- stack_project-2/lib/stack.js
+++ stack_project-2/lib/stack.js
@@ -21,11 +21,47 @@
 // -----------
 
 class Node {
-
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
+  constructor(){
+    this.length = 0;
+    this.top = null;
+    this.bottom = null;
+  }
 
+  push(value){
+    let node = new Node(value);
+    node.next = this.top;
+    if(this.size() === 0){
+      [this.bottom, this.top] = [node, node];
+    }else {
+      this.top = node;
+    }
+
+    this.length++;
+    return this.size();
+  }
+
+  pop(){
+    if(this.size() === 0) return null;
+    let removed = this.top;
+    if(this.size() === 1) {
+      [this.bottom, this.top] = [null, null];
+    } else {
+      this.top = removed.next;
+    }
+    this.length--;
+    return removed.value;
+  }
+
+  size(){
+    return this.length;
+  }
 }
 
 exports.Node = Node;
