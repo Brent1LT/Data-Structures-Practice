a6959b15f91a7d507a3063f288fd974792a7a5cb
Brent Bumann <brent.bumann@aol.com>
2019-05-20 12:03:47 -0700

    Finish binary tree leetcode problem

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,20 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
+  if (preorder.length === 0) return null;
 
+  let root = preorder[0];
+  let rootNode = new TreeNode(root);
+
+  let midIndex = inorder.indexOf(root);
+  let leftInorder = inorder.slice(0, midIndex);
+  let rightInorder = inorder.slice(midIndex + 1);
+
+  let leftPreorder = preorder.filter(el => leftInorder.includes(el));
+  let rightPreorder = preorder.filter(el => rightInorder.includes(el));
+
+  rootNode.left = buildTree(leftPreorder, leftInorder);
+  rootNode.right = buildTree(rightPreorder, rightInorder);
+
+  return rootNode;
 }
