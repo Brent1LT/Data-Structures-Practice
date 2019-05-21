c4bfdfae461e24e96c6a794e0d477a8fba8e611f
Brent Bumann <brent.bumann@aol.com>
2019-05-21 13:05:57 -0700

    Add graph project

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -17,4 +17,24 @@ const heightCheck = root => {
   if (!root) return -1;
 
   return 1 + Math.max(heightCheck(root.left), heightCheck(root.right));
-}
+};
+
+
+// const https = require('https');
+// /*
+//  * Complete the function below.
+//  * Use console.log to print the result, you should not return from the function.
+//  */
+// function getNumberOfMovies(substr) {
+//   /*
+//    * Endpoint: "https://jsonmock.hackerrank.com/api/movies/search/?Title=substr"
+//    */
+//   let req = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`;
+//   https.get(req, res => {
+//     res.setEncoding('utf8');
+//     res.on('data', function (body) {
+//       let result = JSON.parse(body)
+//       console.log(result.total)
+//     });
+//   });
+// }
