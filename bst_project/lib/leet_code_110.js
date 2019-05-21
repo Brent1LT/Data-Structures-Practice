6febfadbaca719090918bc28aed97ada0c784c6d
Brent Bumann <brent.bumann@aol.com>
2019-05-20 21:10:47 -0700

    Finish isBalanced

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,18 @@
 
 
 function isBalanced(root) {
+  if (!root) return true;
+  let rightHeight = heightCheck(root.right);
+  let leftHeight = heightCheck(root.left);
+  if (Math.abs(rightHeight - leftHeight) > 1) {
+    return false;
+  } else {
+    return (isBalanced(root.left) && isBalanced(root.right));
+  }   
+}
 
+const heightCheck = root => {
+  if (!root) return -1;
+
+  return 1 + Math.max(heightCheck(root.left), heightCheck(root.right));
 }
