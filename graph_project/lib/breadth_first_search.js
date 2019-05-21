dd72ad11000458a4e8488173df2e843cde75781c
Brent Bumann <brent.bumann@aol.com>
2019-05-21 13:15:10 -0700

    Finish breadth first graph search

--- graph_project/lib/breadth_first_search.js
+++ graph_project/lib/breadth_first_search.js
@@ -1,5 +1,17 @@
 function breadthFirstSearch(startingNode, targetVal) {
+  if(!startingNode) return null;
+  let q = [startingNode];
+  let visited = new Set();
+  while(q.length){
+    let current = q.shift();
+    if(current.val === targetVal) return current;
+    visited.add(current);
+    current.neighbors.forEach(node => {
+      if(!visited.has(node)) q.push(node);
+    });
+  }
 
+  return null;
 }
 
 module.exports = {
