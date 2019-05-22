47f35adc3682af2dcd3401128f187da679d49379
Brent Bumann <brent.bumann@aol.com>
2019-05-21 17:20:40 -0700

    Finish max value problem

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,5 +1,12 @@
 function maxValue(node, visited=new Set()) {
+  if(!node) return 0;
+  if (visited.has(node.val)) return;
 
+  visited.add(node.val);
+  node.neighbors.forEach(child => {
+    maxValue(child, visited);
+  });
+  return Math.max(...visited.values());
 }
 
 module.exports = {
