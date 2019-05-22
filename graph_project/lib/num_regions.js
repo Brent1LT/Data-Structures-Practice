0d9dd62d281bdb9f27d9237aa1597982396d692a
Brent Bumann <brent.bumann@aol.com>
2019-05-21 22:22:45 -0700

    Finish numRegions problem

--- graph_project/lib/num_regions.js
+++ graph_project/lib/num_regions.js
@@ -1,5 +1,26 @@
 function numRegions(graph) {
+  let visited = new Set();
+  let regions = 0;
+  Object.keys(graph).forEach(node => {
+    if(!visited.has(node)){
+      regions += 1;
+      addRegion(graph, node, visited);
+    }
+  });
   
+  return regions;
+}
+
+function addRegion(graph, node, visited){
+  let stack = [node];
+
+  while(stack.length){
+    let current = stack.pop();
+    if(visited.has(current)) continue;
+    
+    visited.add(current);
+    stack.push(...graph[current]);
+  }
 }
 
 module.exports = {
