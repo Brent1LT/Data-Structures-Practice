61e552698190886f2f2f0c0af9510ffec00b47e4
Brent Bumann <brent.bumann@aol.com>
2019-05-22 19:40:55 -0700

    Finish heap class and is max heap problem

--- heaps_project/lib/is_heap.js
+++ heaps_project/lib/is_heap.js
@@ -1,6 +1,14 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
+  if(idx >= array.length - 1) return true;
+  let leftIdx = idx * 2;
+  let rightIdx = idx * 2 + 1;
+  let right = array[rightIdx];
+  let left = array[leftIdx];
+  if(!right) right = -Infinity;
 
+  if(left > array[idx] || right > array[idx]) return false;
+  return isMaxHeap(array, idx + 1);
 }
 
 
--- heaps_project/lib/max_heap.js
+++ heaps_project/lib/max_heap.js
@@ -1,7 +1,70 @@
 class MaxHeap {
-    
+  constructor(){
+    this.array = [null];
+  }
+
+  insert(val){
+    this.array.push(val);
+
+    this.siftUp(this.array.length - 1);
+  }
+
+  getParent(idx){
+    return Math.floor(idx / 2);
+  }
+
+  getLeftChild(idx){
+    return idx * 2;
+  }
+
+  getRightChild(idx){
+    return idx * 2 + 1;
+  }
+
+  siftUp(idx){
+    if(idx === 1) return;
+    let parentIdx = this.getParent(idx);
+    let currentVal = this.array[idx];
+    let parentVal = this.array[parentIdx];
+
+    if(currentVal > parentVal){
+      [this.array[parentIdx], this.array[idx]] = [currentVal, parentVal];
+    }
+    this.siftUp(parentIdx);
+  }
+
+  siftDown(idx){
+    let leftChild = this.getLeftChild(idx);
+    let rightChild = this.getRightChild(idx);
+    let leftVal = this.array[leftChild];
+    let rightVal = this.array[rightChild];
+    let currentVal = this.array[idx];
+
+    if(!leftVal) leftVal = -Infinity;
+    if(!rightVal) rightVal = -Infinity;
+    if(currentVal > leftVal && currentVal > rightVal) return;
+
+    let swapIdx;
+    if(leftVal > rightVal){
+      swapIdx = leftChild;
+    }else {
+      swapIdx = rightChild;
+    }
+    [this.array[swapIdx], this.array[idx]] = [this.array[idx], this.array[swapIdx]];
+    this.siftDown(swapIdx);
+  }
+
+  deleteMax(){
+    if (this.array.length === 1) return null;
+    if (this.array.length === 2) return this.array.pop();
+    let removed = this.array[1];
+
+    this.array[1] = this.array.pop();
+    this.siftDown(1);
+    return removed;
+  }
 }
 
 module.exports = {
-    MaxHeap
+  MaxHeap
 };
