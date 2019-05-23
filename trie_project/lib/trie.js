c96bd003c54ce8e2e3c362d6cd626215a0708c6a
Brent Bumann <brent.bumann@aol.com>
2019-05-23 15:52:16 -0700

    Finish Trie data structure implementation

--- trie_project/lib/trie.js
+++ trie_project/lib/trie.js
@@ -1,13 +1,72 @@
 class Node {
   constructor(){
     this.children = {};
+    this.isTerminal = false;
   }
 }
 
 class Trie {
   constructor(){
     this.root = new Node();
-    
+  }
+
+  insertRecur(word, root = this.root){
+    if(word.length === 0) return;
+
+    let letter = word[0];
+    if(!(letter in root.children)){
+      root.children[letter] = new Node();
+    }
+
+    if(word.length === 1){
+      root.children[letter].isTerminal = true;
+    }else{
+      this.insertRecur(word.slice(1), root.children[letter]);
+    }
+  }
+
+  insertIter(word, root = this.root){
+    let q = word.split('');
+    let currentRoot = root;
+    while(q.length){
+      let current = q.shift();
+      
+      if(!(current in currentRoot.children)) currentRoot.children[current] = new Node();
+      currentRoot = currentRoot.children[current];
+      if(!q.length){
+        currentRoot.isTerminal = true;
+        continue;
+      }
+    }
+  }
+
+  searchRecur(word, root = this.root){
+    if(word.length === 0){
+      if(root.isTerminal){
+        return true;
+      }else return false;
+    }
+
+    let letter = word[0];
+    if (letter in root.children) {
+      return this.searchRecur(word.slice(1), root.children[letter]);
+    }else return false;
+  }
+
+  searchIter(word, root = this.root){
+    let q = word.split('');
+    let currentRoot = root;
+
+    while(q.length){
+      let current = q.shift();
+
+      if (!(current in currentRoot.children)) return false;
+      currentRoot = currentRoot.children[current];
+    }
+
+    if (currentRoot.isTerminal) {
+      return true;
+    } else return false;
   }
 }
 
