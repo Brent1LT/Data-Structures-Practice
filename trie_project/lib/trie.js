5e2634e7e501bfa2f669f5b4abf1a0008fe9a80a
Brent Bumann <brent.bumann@aol.com>
2019-05-28 10:47:43 -0700

    Add dijkstras algo

--- trie_project/lib/trie.js
+++ trie_project/lib/trie.js
@@ -116,3 +116,54 @@ module.exports = {
     Node,
     Trie
 };
+
+
+function dijkstras(graph, source){
+  let distance = {};
+  debugger
+  for (let node in graph){
+    distance[node] = Infinity;
+  }
+  distance[source] = 0;
+
+  let unvisited = new Set(Object.keys(graph));
+  // let previous = {};
+
+  while(unvisited.size > 0){
+    let currentNode = getSmallestNode(unvisited, distance);
+    unvisited.delete(currentNode);
+
+    for(let neighbor in graph[currentNode]){
+      let distanceToNeighbor = graph[currentNode][neighbor];
+      let totalDistance = distanceToNeighbor + distance[currentNode];
+
+      if(totalDistance < distance[neighbor]){
+        distance[neighbor] = totalDistance;
+        // previous[neighbor] = currentNode;
+      }
+    }
+  }
+
+  return distance;
+}
+
+function getSmallestNode(unvisited, distance){
+  return Array.from(unvisited).reduce((minNode, node) =>{
+    if(distance[node] < distance[minNode]){
+      return node;
+    }else {
+      return minNode;
+    }
+  });
+}
+
+let graph = {
+  'a': {'c': 1, 'b': 7},
+  'b': {'a': 7, 'd': 12, 'e': 13},
+  'c': {'a': 1, 'd': 20, 'f': 4},
+  'd': {'b': 12, 'c': 20, 'e': 5},
+  'e': {'b': 13, 'd': 5, 'f': 9},
+  'f': {'c': 4, 'e': 9}
+};
+
+
